6c96fc4a2fb3097cb915553dbfb1960c93f9fcc5
Blue Saber <saber@ncu.edu.tw>
2015-07-04 11:15:39 +0800

    Add componentScan support.

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
+'use strict';
+
+var fs = require('fs');
+var path = require('path');
+
 function Repository() {
 	this.providers = {};
+}
+
+function endsWith(str, suffix) {
+    return str.indexOf(suffix, str.length - suffix.length) !== -1;
 }
 
 Repository.prototype.add = function (key, obj) {
@@ -7,6 +16,39 @@ Repository.prototype.add = function (key, obj) {
 		throw new Error('Duplicate key: ' + key);
 	} else {
 		this.providers[key] = obj;
+	}
+}
+
+Repository.prototype.get = function (key) {
+	if (this.providers.hasOwnProperty(key)) {
+		return this.providers[key].obj;
+	}
+	return undefined;
+}
+
+Repository.prototype.componentScan = function (basedir, dirs) {
+	var self = this;
+
+	for (var i = 0; i < dirs.length; i++) {
+		var scandir = path.join(basedir, dirs[i]);
+
+		fs.readdirSync(scandir).forEach(function (file) {
+			var filepath = path.join(scandir, file);
+			var stats = fs.statSync(filepath);
+
+			if (stats.isFile()) {
+				if (endsWith(file, ".js")) {
+					var fname = file.substr(0, file.indexOf('.'));
+					filepath = path.join(scandir, fname);
+					
+					// self.addItem(fname, require(filepath));
+					new Instance (fname, require(filepath))
+				}
+			} else if (stats.isDirectory()) {
+				// self.addItem(file, require(filepath));
+				new Instance (file, require(filepath))
+			}
+		});
 	}
 }
 
@@ -51,7 +93,11 @@ Repository.prototype.autowire = function () {
 					}
 
 					if (injectable) {
-						item.obj.inject(args);
+						console.log('Inject ' + key);
+
+						if (typeof item.obj.inject === 'function') {
+							item.obj.inject(args);
+						}
 						item.injected = true;
 						haveInject = true;
 					}
@@ -66,7 +112,7 @@ Repository.prototype.autowire = function () {
 
 var repos = new Repository();
 
-function Instance(o) {
+function Instance(f, o) {
 	this.obj = o;
 	this.injected = false;
 
@@ -77,15 +123,25 @@ function Instance(o) {
 		}
 	} else if (typeof(this.obj.provide) === 'string') {
 		repos.add (this.obj.provide, this);
+	} else {
+		repos.add (f, this);
 	}
 }
 
-exports.addItem = function addItem(obj) {
-	new Instance (obj);
+exports.addItem = function addItem(file, obj) {
+	new Instance (file, obj);
 }
 
 exports.autowire = function () {
 	repos.autowire();
 }
 
-exports.version = "0.0.1";
+exports.componentScan = function (basedir, dirs) {
+	return repos.componentScan(basedir, dirs);
+}
+
+exports.get = function (key) {
+	return repos.get(key);
+}
+
+exports.version = "0.0.2";
